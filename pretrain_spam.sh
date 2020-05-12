159dff6e3079f48dea67acc59b8011f31a93c0aa
Wuxialang zhou <wuxialangzhou@Wuxialangs-MacBook-Pro.local>
2020-05-12 11:30:45 +0800

    x

---
+++ pretrain_spam.sh
@@ -0,0 +1,12 @@
+python build_pretraining_dataset.py \
+    --corpus-dir /mnt/data1/xialang/corpus/electra_pretrain_data \
+    --vocab-file /mnt/data1/xialang/projects/chn_electra_small/vocab.txt \
+    --output-dir ../electra_pretrain_data \
+    --max-seq-length 64 \
+    --num-processes 16
+
+
+python run_pretraining.py \
+    --data-dir /mnt/data1/xialang/corpus/electra_pretrain_data \
+    --model-name electra_small
+    --
